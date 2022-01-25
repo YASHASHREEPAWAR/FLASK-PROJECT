196635167e8d9f7729a5b1f61850784451fe8138
YASHASHREEPAWAR <yashashreepawar11@gmail.com>
2022-01-25 18:24:30 +0530

    ADD

--- server.py
+++ server.py
@@ -2,24 +2,12 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-print(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hellooooooooooooooooo....'
+def home():
+    return render_template('/index.html')
 
-@app.route('/blogs')
-def blogs():
-    return 'This is a blog.'
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/blog/2020/dog')
-def blog2():
-    return 'This is a blog about dog.'
-
-@app.route('/aboutus.html')
-def about():
-    return render_template('aboutus.html')
-
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
